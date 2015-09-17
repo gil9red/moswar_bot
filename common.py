5a177c4f7ef0e53bcc1f204f2aa1666b04685e3c
Ilya Petrash <ip1992@inbox.ru>
2015-09-17 22:13:26 +0500

    Изменен формат логируемых сообщений

--- common.py
+++ common.py
@@ -12,8 +12,7 @@ def get_logger(name, file='log.txt', encoding='utf8'):
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
 
-    # formatter = logging.Formatter('%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
-    formatter = logging.Formatter('[%(asctime)s] %(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s')
+    formatter = logging.Formatter('[%(asctime)s] %(filename)s[LINE:%(lineno)d] %(levelname)-8s %(message)s')
 
     fh = logging.FileHandler(file, encoding=encoding)
     fh.setLevel(logging.DEBUG)

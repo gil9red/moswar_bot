ce8787b7c2c53c0d4d67650b555f0c3bf223e450
ipetrash <gil9red@gmail.com>
2015-10-02 12:27:25 +0500

    -

--- main.py
+++ main.py
@@ -23,24 +23,43 @@ if __name__ == '__main__':
 
     # from PySide.QtWebKit import *
     # view = QWebView()
-    # # view.setHtml(open('Ограбление корованов. Пустыня.htm', encoding='utf-8').read())
-    # view.setHtml("""        <div class="fighter2">
-    #       <span class="user ">
-    #            <i class="npc" title="Горожанин"></i>
-    #            <a onclick="return AngryAjax.goToUrl(this, event);" href="http://www.moswar.ru/player/38325/"> savteam</a>
-    #            <span class="level">[7]</span>
-    #        </span>
-    #      </div>""")
-    # # from PySide.QtCore import *
-    # # timer = QTimer()
-    # # timer.setSingleShot(True)
-    # # timer.start(3000)
-    # # loop = QEventLoop()
-    # # timer.timeout.connect(loop.quit)
-    # # loop.exec_()
+    # view.setHtml(open('Проигранный бой ВсехПорву [9] vs. Draconzver [10] .htm', encoding='utf-8').read())
+    # # view.setHtml("""        <div class="fighter2">
+    # #       <span class="user ">
+    # #            <i class="npc" title="Горожанин"></i>
+    # #            <a onclick="return AngryAjax.goToUrl(this, event);" href="http://www.moswar.ru/player/38325/"> savteam</a>
+    # #            <span class="level">[7]</span>
+    # #        </span>
+    # #      </div>""")
+    # from PySide.QtCore import *
+    # timer = QTimer()
+    # timer.setSingleShot(True)
+    # timer.start(3000)
+    # loop = QEventLoop()
+    # timer.timeout.connect(loop.quit)
+    # loop.exec_()
     #
     # doc = view.page().mainFrame().documentElement()
-    # print(doc.findFirst('.fighter2 a').attribute('href'))
     #
+    #
+    # import re
+    # pattern = re.compile(r'Победитель: (.+) \[[\d]+\]')
+    # pattern2 = re.compile(r'(.+) \[[\d]+\]')
+    #
+    # css_path = '#personal .name'
+    # name = doc.findFirst(css_path)
+    # match = pattern2.search(name.toPlainText())
+    # print('"{}"'.format(match.group(1)))
+    #
+    # # print('"{}"'.format(doc.findFirst('.fighter2 a').toPlainText()))
+    # # result = doc.findFirst('.result')
+    # # if result.isNone():
+    # #     raise Exception('Не найден result')
+    # # result_text = result.toPlainText()
+    # # match = pattern.search(result_text)
+    # # if match is None:
+    # #     raise Exception('Не удалось найти победителя драки')
+    # # print('Победитель: "{}"'.format(match.group(1)))
+    # #
     # # view.show()
     # # app.exec_()
